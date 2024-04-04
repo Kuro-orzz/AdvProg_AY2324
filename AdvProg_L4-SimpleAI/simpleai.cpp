4ab1948646addd4ac182f85e762a4576bb80b8de
admin <23020674@vnu.edu.vn>
2024-04-04 23:09:27 +0700

    update

--- AdvProg_L4-SimpleAI/simpleai.cpp
+++ AdvProg_L4-SimpleAI/simpleai.cpp
@@ -152,7 +152,7 @@ bool isWholeWord(const string& mask)
 //     bool answer = true;
     //Write your code here
     for(char c : mask)
-        if(c == '_')
+        if(c == '-')
             return false;
     return true;
 }
